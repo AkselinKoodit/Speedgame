68cef96cf07e656e301ea92a4321a2b8304affb4
Akseli <s2100140@edu.bc.fi>
2021-03-09 22:17:50 +0200

    a loot of improvements, such as sounds and bg music

--- main.js
+++ main.js
@@ -7,6 +7,31 @@ let close = document.getElementById("close");
 let active = 0;
 let speed = 1200;
 
+let clickSound;
+let mistakeSound;
+let opener;
+let theme;
+
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function () {
+    this.sound.play();
+  };
+  this.stop = function () {
+    this.sound.pause();
+  };
+}
+
+function playTheme() {
+  theme = new sound("guerrillaRadio.mp3");
+  theme.play();
+}
+
 document.getElementById("stop").classList.add("invisible");
 
 buttons[0].onclick = function () {
@@ -25,10 +50,13 @@ buttons[3].onclick = function () {
 const clicked = (i) => {
   console.log("clicked:", i);
   if (i === active) {
+    //getting the click sound:
+    clickSound.play();
     score++;
     scoredisplay.textContent = score;
     speed *= 0.9;
   } else {
+    mistakeSound.play();
     console.log("Missed!");
     endGame();
   }
@@ -40,7 +68,17 @@ const startGame = () => {
 
   document.getElementById("stop").classList.remove("invisible");
 
+  //making circles clickable:
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.add("clickMe");
+  }
+
+  //constructing sound:
+  clickSound = new sound("ballBlib.wav");
+  mistakeSound = new sound("mistake.wav");
+
   console.log("game started");
+  // openingSound.play();
   let nextActive = pickNew(active);
 
   buttons[nextActive].classList.toggle("active");
@@ -70,7 +108,18 @@ const endGame = () => {
   clearTimeout(timer);
   console.log("game over! Your score is " + score);
   overlay.style.visibility = "visible";
-  gameover.textContent = "Your score: " + score;
+  if (score < 2) {
+    gameover.textContent = "Pathetic! Your score: " + score;
+  } else if (score < 4) {
+    gameover.textContent = "Your score: " + score + " You can do better.";
+  } else if (score < 6) {
+    gameover.textContent = "Your score: " + score + ", not bad at all!";
+  } else if (score < 9) {
+    gameover.textContent = "Awesome! You got " + score + " points!";
+  } else {
+    gameover.textContent =
+      "H-How did you that?!?!?! You got ridiculous score of " + score + "!!!";
+  }
 };
 
 // close.addEventListener("click", reloadGame);
